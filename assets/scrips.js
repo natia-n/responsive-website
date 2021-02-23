9f18902a0697f4ab63881567c57745ee6b832ec9
Natia Nakashidze <n.nakashidze09@gmail.com>
2021-02-23 23:23:13 +0400

    header responsive

---
+++ assets/scrips.js
@@ -0,0 +1,6 @@
+const manuImg = document.getElementById('manu-img');
+const manu = document.getElementById('manu');
+
+manuImg.addEventListener('click', function(){
+    manu.classList.toggle('show');       
+})
