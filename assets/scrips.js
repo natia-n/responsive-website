06625efa61a48381b42d204292472b9a8302bc3d
Natia Nakashidze <n.nakashidze09@gmail.com>
2021-02-25 20:42:47 +0400

    section-3-is responsivi dasrulda

--- assets/scrips.js
+++ assets/scrips.js
@@ -4,3 +4,11 @@ const manu = document.getElementById('manu');
 manuImg.addEventListener('click', function(){
     manu.classList.toggle('show');       
 })
+
+const absolut1 = document.getElementById('absolut-1');
+const page3 = document.getElementById('page-3');
+console.log(page3.display);
+if(absolut1.style.right < 0){
+    page3.style.display = 'block';
+    console.log(page3.style.display);
+}
